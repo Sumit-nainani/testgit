624a0a70b4edd846e5ab6ca608e17ecd47cf8a75
Sumit-nainani <120CS0194@nitrkl.ac.in>
2025-08-18 02:04:18 +0530

    "init"

---
+++ Main.java
@@ -0,0 +1,34 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int sum = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int mod = 1000000007;
+        long[] dp = new long[sum + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= sum; i++) {
+            for (int x : arr) {
+                if (x <= i) {
+                    dp[i] += (dp[i - x]) %= mod;
+                }
+            }
+        }
+        //printing test
+        System.out.println(dp[sum]);
+
+    }
+}
